a2fd683f5778721ed43a1095101770b7ec690084
Akshay <jainakshay058@gmail.com>
2023-06-25 21:26:06 +0530

    Update with Testng

--- src/demo/Login.java
+++ src/demo/Login.java
@@ -1,11 +1,14 @@
 package demo;
 
-import static org.junit.Assert.*;
-import org.junit.jupiter.api.*;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+//import static org.junit.Assert.*;
+import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.junit.*;
+//import org.junit.jupiter.api.*;
+//import org.junit.After;
+//import org.junit.Assert;.....
+//import org.junit.Before;
+//import org.junit.Test;
 //import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,25 +16,31 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {
-	WebDriver driver;
+//	WebDriver driver = new ChromeDriver();
+	private static WebDriver driver;
 	
-	@Before
+	@Test (priority=1)
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\flyma\\OneDrive\\Documents\\selenium\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://sakshingp.github.io/assignment/login.html");
+		driver.manage().window().maximize();
+
 		
 	}
-  @Test 
+  @Test (priority=2)
 	public void test_title() throws InterruptedException{
 		// TODO Auto-generated method stub
-		driver.manage().window().maximize();
 		String actualTitle = driver.getTitle();
 //		Assert.assertEquals("Demo App",actualTitle);
-        Assertions.assertEquals("Demo App", actualTitle);
+        Assert.assertEquals("Demo App", actualTitle);
 		System.out.println(" Title Matched! Test case Passed!");
 
-
+  }
+  
+  @Test (priority=3)
+	public void test_login_details() throws InterruptedException{
+	  
 		
 //		 Enter valid user_name and password to login.........
 			driver.findElement(By.id("username")).sendKeys("akshay");
@@ -53,23 +62,38 @@ public class Login {
 			// Click on login....
 			driver.findElement(By.id("log-in")).click();
 			Thread.sleep(2000);
+  }
+  @Test (priority=4)
+	public void test_url() throws InterruptedException{
+	  
+
 			
 			// save current url and compare with original url....
 			String act_url = driver.getCurrentUrl();
 			System.out.println("Current url: "+act_url);
 			Assert.assertEquals(act_url,"https://sakshingp.github.io/assignment/home.html");
 			System.out.println("Home url Matched! Test Case Passed");
+  }
+  
+  @Test (priority=5)
+	public void test_table_clickable() throws InterruptedException{
+
 			
 			
 			// Navigate to the transaction table.......
 		    WebElement transactionTable = driver.findElement(By.id("transactionsTable"));
 		    transactionTable.click();
 		    
-		    //Locate the AMOUNT header element and click it........
-		    WebElement amountHeader = driver.findElement(By.id("amount"));
-		    amountHeader.click();
-		    
-			Thread.sleep(1000);
+  }
+  
+  @Test (priority=6)
+	public void test_sort() throws InterruptedException{
+
+	//Locate the AMOUNT header element and click it........
+	  WebElement amountHeader = driver.findElement(By.id("amount"));
+	    amountHeader.click();
+	    
+		Thread.sleep(1000);
 			
 			// Extract the value of the onclick attribute to check sort function execute or not ......
 			String onclickValue = amountHeader.getAttribute("onclick");
@@ -82,12 +106,14 @@ public class Login {
 			System.out.println(" Amount Header Sorted! Test Case Passed!");
 
 		   
-			Thread.sleep(2000);
+			Thread.sleep(1000);
+//			driver.quit();
+
 			
   }
 
 
-  @After
+  @Test (priority=7)
 public void closeBrowser() {
 	driver.quit();
 }
